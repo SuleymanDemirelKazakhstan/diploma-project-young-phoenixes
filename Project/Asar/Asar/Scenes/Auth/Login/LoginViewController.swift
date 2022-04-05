e53f431363566137ab0a3ecc4a59e3b791fbc480
Itsnotbexxx <nurpeis.beka@gmail.com>
2022-04-06 05:23:02 +0600

    add Localizable.strings

--- Project/Asar/Asar/Scenes/Auth/Login/LoginViewController.swift
+++ Project/Asar/Asar/Scenes/Auth/Login/LoginViewController.swift
@@ -15,9 +15,19 @@ protocol LoginViewControllerDelegate {
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var login_btn: UIButton!
+    @IBOutlet weak var forget_btn: UIButton!
+    @IBOutlet weak var register_btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let login = NSLocalizedString("log_in", comment: "")
+        login_btn.setTitle(login, for: .normal)
+        let forgetBtn = NSLocalizedString("forget_password", comment: "")
+        forget_btn.setTitle(forgetBtn, for: .normal)
+        let register = NSLocalizedString("register", comment: "")
+        register_btn.setTitle(register, for: .normal)
     }
     
     @IBAction private func loginDidTap() {
