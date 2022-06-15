ffbbb3d7514c09b07aaf762e803e43f558a14b29
Itsnotbexxx <nurpeis.beka@gmail.com>
2022-06-16 03:07:33 +0600

    fix

--- Project/Asar/Asar/Scenes/Profile/New Group1/Controller/ProfileInformation/ProfileInformationViewController.swift
+++ Project/Asar/Asar/Scenes/Profile/New Group1/Controller/ProfileInformation/ProfileInformationViewController.swift
@@ -10,10 +10,10 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 
-class ProfileInformationViewController: UIViewController {
 
-    let defaults = UserDefaults.standard
-    let email = Auth.auth().currentUser?.uid
+class ProfileInformationViewController: UIViewController {
+    let db = Firestore.firestore()
+    
     
     private let photoView: UIImageView = {
         let photoView = UIImageView(image: UIImage(named: "ava")!)
@@ -35,6 +35,20 @@ class ProfileInformationViewController: UIViewController {
         return label
     }()
     
+    private let phonenumberView: UIView = {
+        let innerView = UIView()
+        innerView.backgroundColor = .white
+        innerView.layer.cornerRadius = 10
+        return innerView
+    }()
+    
+    private let phoneNumberTextField: UITextField = {
+          let textField = UITextField()
+        textField.placeholder = "Phone Number"
+        textField.backgroundColor = UIColor.white
+          return textField
+      }()
+    
     private let numberView: UIView = {
         let innerView = UIView()
         innerView.backgroundColor = .white
@@ -51,6 +65,26 @@ class ProfileInformationViewController: UIViewController {
         super.viewDidLoad()
         navigationItem.title = "Profile"
         view.backgroundColor = .systemGray6
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "accept"),
+            style: .done,
+            target: self,
+            action: #selector(didTapAccept))
+        phoneNumberTextField.delegate = self
+        let docRef = db.collection("users").document(Auth.auth().currentUser?.email ?? "")
+        docRef.getDocument { snapshot, error in
+            guard let data = snapshot?.data(), error == nil else {
+                return
+            }
+            
+            guard let phoneNumber = data["phoneNumber"] as? String else{
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.phoneNumberTextField.text = phoneNumber
+            }
+        }
         setup()
         updateUI()
     }
@@ -75,9 +109,21 @@ class ProfileInformationViewController: UIViewController {
             $0.edges.equalToSuperview().inset(16)
         }
         
+        view.addSubview(phonenumberView)
+        phonenumberView.snp.makeConstraints {
+            $0.top.equalTo(mailView.snp.bottom).offset(16)
+            $0.left.right.equalToSuperview().inset(16)
+            $0.height.equalTo(56)
+        }
+        
+        phonenumberView.addSubview(phoneNumberTextField)
+        phoneNumberTextField.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(16)
+        }
+        
         view.addSubview(numberView)
         numberView.snp.makeConstraints {
-            $0.top.equalTo(mailView.snp.bottom).offset(16)
+            $0.top.equalTo(phonenumberView.snp.bottom).offset(16)
             $0.left.right.equalToSuperview().inset(16)
             $0.height.equalTo(56)
         }
@@ -89,12 +135,28 @@ class ProfileInformationViewController: UIViewController {
       
     }
     
+    @objc private func didTapAccept(){
+    }
+    
     private func updateUI() {
        guard let user = Auth.auth().currentUser else { return }
         mailLabel.text = user.email
         numberLabel.text = user.uid
+//        phoneNumberTextField.text = user.email
      }
     
-    
+    func saveData(phoneNumber: String){
+        let docRef = db.collection("users").document(Auth.auth().currentUser?.email ?? "")
+        docRef.setData(["phoneNumber": phoneNumber])
+    }
 }
 
+
+extension ProfileInformationViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let phoneNumber = textField.text, !phoneNumber.isEmpty{
+            saveData(phoneNumber: phoneNumber)
+        }
+        return true
+    }
+}
---
+++ Project/Asar/Asar/Scenes/Profile/New Group1/Model/UserProfile.swift
@@ -0,0 +1,13 @@
+//
+//  UserProfile.swift
+//  Asar
+//
+//  Created by Бексултан Нурпейс on 16.06.2022.
+//
+
+import Foundation
+
+struct UserProfile{
+    var id: String = UUID().uuidString
+    let phoneNumber: String?
+}
